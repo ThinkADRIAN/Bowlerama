710d7b817d90696280a7eb514301f5912276bc90
Adrian Cunanan <adrian@adriancunanan.com>
2015-02-25 09:33:56 -0500

    Update isStrike and isSpare.  Improve Commenting.

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -13,28 +13,55 @@ class Frame < ActiveRecord::Base
     self.save
   end
 
-  def setScore(number_of_pins)
-    self.updateScore(number_of_pins)
+  def setScore(bowled_pins)
+    self.updateScore(bowled_pins)
 
+    # Handle 2 strikes in a row
     if getFrame(-1).isStrike? and getFrame(-2).isStrike?
+
+      # First stroke of current frame
     	if second_stroke.nil?
-      	getFrame(-2).updateScore(number_of_pins)
+      	getFrame(-2).updateScore(bowled_pins)
       end
+
+      # Second stroke of current frame
       if extra_stroke.nil?
-       getFrame(-1).updateScore(number_of_pins)
+       getFrame(-1).updateScore(bowled_pins)
      	end
+
+    # Handle strike in previous frame on completion of current frame
     elsif getFrame(-1).isStrike? and extra_stroke.nil?
-      getFrame(-1).updateScore(number_of_pins)
+      getFrame(-1).updateScore(bowled_pins)
+
+    # Handle spare in previous frame after first stroke of current frame
     elsif getFrame(-1).isSpare? and second_stroke.nil?
-      getFrame(-1).updateScore(number_of_pins)
+      getFrame(-1).updateScore(bowled_pins)
     end
   end
 
   def isStrike?
-    self.first_stroke == "X" || self.second_stroke == "X" ||  self.extra_stroke == "X"
+    # Handle strike in first stroke of frames 1 through 10
+    if second_stroke.nil?
+      self.first_stroke == "X"
+
+    # Handle strike in second stroke of frame 10
+    elsif extra_stroke.nil?
+      self.second_stroke == "X"
+
+    # Handle strike in extra stroke of frame 10
+    else 
+      self.extra_stroke == "X"
+    end
   end
 
   def isSpare?
-    self.second_stroke == "/" ||  self.extra_stroke == "/"
+    # Handle spare in frames 1 through 10
+    if extra_stroke.nil?
+      self.second_stroke == "/"
+
+    # Handle spare in extra stroke of frame 10
+    else
+      self.extra_stroke == "/"
+    end
   end
 end
