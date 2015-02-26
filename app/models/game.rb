578e1761d680377caddfca4e95be669b7f5f25e5
Adrian Cunanan <adrian@adriancunanan.com>
2015-02-26 15:42:50 -0500

    DRY Refactor

--- app/models/game.rb
+++ app/models/game.rb
@@ -14,7 +14,6 @@ class Game < ActiveRecord::Base
     	self.frames << frame
     	i += 1
     }
-
     self.save
   end
 
@@ -52,7 +51,6 @@ class Game < ActiveRecord::Base
         roll_value = randomizePinCountForSecondThrow
       end
     end
-    #insertRoll(roll_value)
 	end
 
 	def incrementFrameCount!
@@ -197,7 +195,7 @@ class Game < ActiveRecord::Base
     end
 
     calculateFrameScores!
-    calculateTotalScore!
+    #calculateTotalScore!
   end
 
   def isLastStrokeStrike?
@@ -252,6 +250,12 @@ class Game < ActiveRecord::Base
     self.frame_stroke == -1
   end
 
+  def insertRoll(value)
+    self.rolls_will_change!
+    self.rolls << value
+    self.save!
+  end
+
   def getFrame(frame_number)
   	self.frames.each { |frame| 
   		if frame.frame_number == frame_number
@@ -274,8 +278,22 @@ class Game < ActiveRecord::Base
     self.save
   end
 
-  def getNextRoll(index)
-    self.rolls[index+1]
+  def convertRollValue(frame_value)
+    if (frame_value == "X") || (frame_value =="/")
+      10
+    elsif frame_value.nil?
+      0
+    else
+      i.to_i
+    end
+  end
+
+  def subZeroForNil( value )
+    if value.nil?
+      0
+    else
+      value
+    end
   end
 
   def getNextRollValue(this_frame_number, this_frame_stroke)
@@ -294,95 +312,77 @@ class Game < ActiveRecord::Base
     end
     return next_frame_value
   end
-
-  def convertRollValue(frame_value)
-    if (frame_value == "X") || (frame_value =="/")
-      10
-    elsif frame_value.nil?
-      0
-    else
-      i.to_i
-    end
-  end
-
-  def insertRoll(value)
-    self.rolls_will_change!
-    self.rolls << value
-    self.save!
-  end
-
-  def loadRollValue
-    roll_value = getNextRollValue
-    roll_value = convertRollValue(roll_value)
-    insertRoll(roll_value)
-  end
-
-  def getRollValueFor(index)
-    if !self.rolls.any?
-      roll_value = self.rolls[index]
-    else
-      0
-    end
-  end
-
-  def scoreOpenFrame(this_frame_number)
-    if self.getFrame(this_frame_number).first_stroke.nil?
-      first_roll = 0
-    else
-      first_roll = self.getFrame(this_frame_number).first_stroke.to_i
-    end
-
-    if self.getFrame(this_frame_number).second_stroke.nil?
-      second_roll = 0
-    else
-      second_roll = self.getFrame(this_frame_number).second_stroke.to_i
-    end
-
-    frame_score = first_roll + second_roll 
-  end
-
-  def scoreStrike(this_frame_number,this_frame_stroke)
-    if self.getNextRollValue(this_frame_number, this_frame_stroke).nil?
-      next_roll_value = 0
-    else
-      next_roll_value = self.getNextRollValue(this_frame_number, this_frame_stroke)
-    end
-
+    
+  def getNextFrameNumber(this_frame_number, this_frame_stroke)
     if this_frame_stroke == 1
-      next_frame_stroke = 2
       next_frame_number = this_frame_number
     elsif this_frame_number < 10
       if this_frame_stroke == 2
-        next_frame_stroke = 1
         next_frame_number = this_frame_number + 1
       end
     elsif this_frame_number == 10
       if this_frame_stroke == 2
-        next_frame_stroke = 3
         next_frame_number = this_frame_number
       end
     end
-      
-    if self.getNextRollValue(next_frame_number, next_frame_stroke).nil?
-      next_next_roll_value = 0
-    else
-      next_next_roll_value = self.getNextRollValue(this_frame_number, this_frame_stroke)
+  end
+
+  def getNextStrokeNumber(this_frame_number, this_frame_stroke)
+    if this_frame_stroke == 1
+      next_frame_stroke = 2
+    elsif this_frame_number < 10
+      if this_frame_stroke == 2
+        next_frame_stroke = 1
+      end
+    elsif this_frame_number == 10
+      if this_frame_stroke == 2
+        next_frame_stroke = 3
+      end
     end
+  end
+
+  def scoreOpenFrame(this_frame_number)
+    first_roll = subZeroForNil(self.getFrame(this_frame_number).first_stroke).to_i
+    second_roll = subZeroForNil(self.getFrame(this_frame_number).second_stroke).to_i
+    frame_score = first_roll + second_roll 
+  end
+
+  def scoreBonus(this_frame_number,this_frame_stroke, this_roll_value)
+    if this_roll_value = "X"
+      this_roll_value = scoreStrike(this_frame_number, this_frame_stroke)
+    elsif this_roll_value = "/"
+      this_roll_value = scoreSpare(this_frame_number, this_frame_stroke)
+    else
+      this_roll_value = scoreOpenFrame(this_frame_number)
+    end
+    this_roll_value = convertRollValue(this_roll_value)
+  end
+
+  def scoreStrike(this_frame_number,this_frame_stroke)
+    next_roll_value = subZeroForNil(self.getNextRollValue(this_frame_number, this_frame_stroke))
+
+    next_frame_number = getNextFrameNumber(this_frame_number, this_frame_stroke)
+    next_stroke_number = getNextStrokeNumber(this_frame_number, this_frame_stroke)
+      
+    next_next_roll_value = subZeroForNil(self.getNextRollValue(next_frame_number, next_stroke_number))
+
+    next_roll_value = scoreBonus(next_frame_number, next_stroke_number, next_roll_value)
 
     frame_score = 10 + next_roll_value + next_next_roll_value
   end
 
   def scoreSpare(this_frame_number, this_frame_stroke)
-    if self.getNextRollValue(this_frame_number, this_frame_stroke).nil?
-      next_roll_value = 0
-    else
-      next_roll_value = self.getNextRollValue(this_frame_number, this_frame_stroke)
-    end
+    next_roll_value = subZeroForNil(self.getNextRollValue(this_frame_number, this_frame_stroke))
+
+    next_frame_number = getNextFrameNumber(this_frame_number, this_frame_stroke)
+    next_stroke_number = getNextStrokeNumber(this_frame_number, this_frame_stroke)
+
+    next_roll_value = scoreBonus(next_frame_number, next_stroke_number, next_roll_value)
 
     frame_score = 10 + next_roll_value
   end
 
-  def calculateFrameScore!
+  def calculateFrameScoreFromRolls!
     self.frames.each { |frame|
       index = convertFrameToIndex(frame)
 
@@ -400,99 +400,21 @@ class Game < ActiveRecord::Base
   end
 
   def calculateFrameScores!
+    current_frame = getFrame(self.current_frame)
     
-    i = 1
-
-    while i <= self.current_frame
-      current_frame = self.getFrame(i)
-
-      if current_frame.isStrike?
-        frame_score = self.scoreStrike(current_frame.frame_number, self.current_frame_number)
-      elsif current_frame.isStrike?
-        frame_score = self.scoreSpare(current_frame.frame_number, self.current_frame_number)
-      elsif current_frame.isOpenFrame?
-        frame_score = self.scoreOpenFrame(current_frame.frame_number)
-      end
-
-      self.frames.where(frame_number: current_frame.frame_number ).update_all(frame_score: frame_score)
-
-      self.save
-
-      i += 1
+    if current_frame.isStrike?
+      frame_score = self.scoreStrike(current_frame.frame_number, self.frame_stroke)
+    elsif current_frame.isSpare?
+      frame_score = self.scoreSpare(current_frame.frame_number, self.frame_stroke)
+    elsif current_frame.isOpenFrame?
+      frame_score = self.scoreOpenFrame(current_frame.frame_number)
     end
+    
+    self.frames.where(frame_number: current_frame.frame_number ).update_all(frame_score: frame_score)
+    self.save
   end
 
   def calculateTotalScore!
     self.total_score = self.getFrame(self.current_frame).frame_score + self.getFrame(self.current_frame+1).frame_score
     self.save
   end
-
-  def convertFrameToIndex(frame)
-    frame_number = frame.frame_number
-    case frame_number
-    when 1
-      if self.frame_stroke
-        index = 0
-      elsif !frame.second_stroke.nil?
-        index = 1
-      end
-    when 2
-      if !frame.first_stroke.nil?
-        index = 2
-      elsif !frame.second_stroke.nil?
-        index = 3
-      end
-    when 3
-      if !frame.first_stroke.nil?
-        index = 4
-      elsif !frame.second_stroke.nil?
-        index = 5
-      end
-    when 4
-      if !frame.first_stroke.nil?
-        index = 6
-      elsif !frame.second_stroke.nil?
-        index = 7
-      end
-    when 5
-      if !frame.first_stroke.nil?
-        index = 8
-      elsif !frame.second_stroke.nil?
-        index = 9
-      end
-    when 6
-      if !frame.first_stroke.nil?
-        index = 10
-      elsif !frame.second_stroke.nil?
-        index = 11
-      end
-    when 7
-      if !frame.first_stroke.nil?
-        index = 12
-      elsif !frame.second_stroke.nil?
-        index = 13
-      end
-    when 8
-      if !frame.first_stroke.nil?
-        index = 14
-      elsif !frame.second_stroke.nil?
-        index = 15
-      end
-    when 9
-      if !frame.first_stroke.nil?
-        index = 16
-      elsif !frame.second_stroke.nil?
-        index = 17
-      end
-    when 10
-      if !frame.first_stroke.nil?
-        index = 18
-      elsif !frame.second_stroke.nil?
-        index = 19
-      else
-        index = 20
-      end
-    end
-    index = 21
-  end
-end
